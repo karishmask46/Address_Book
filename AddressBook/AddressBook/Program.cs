e8cbc9cead0cf374f88244a010cdd791f15f79cf
karishmask46 <karishmask.15999@gmail.com>
2022-07-16 21:54:17 +0530

    creating contact

---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Problems");
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact();
+        }
+    }
+}
---
+++ AddressBook/AddressBook/UC1_CreateContact.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+
+{
+    class AddressBook
+    {
+        //Create dictionary to store result
+        Dictionary<string, string> Contactlist = new Dictionary<string, string>();
+        public void AddContact()
+        {
+            Console.WriteLine("first Name:");
+            Contactlist.Add("first Name", Console.ReadLine());
+
+            Console.WriteLine("last Name:");
+            Contactlist.Add("last Name", Console.ReadLine());
+
+            Console.WriteLine("Address:");
+            Contactlist.Add("Address", Console.ReadLine());
+
+            Console.WriteLine("City:");
+            Contactlist.Add("City", Console.ReadLine());
+
+            Console.WriteLine("State:");
+            Contactlist.Add("State", Console.ReadLine());
+
+            Console.WriteLine("Zip:");
+            Contactlist.Add("Zip", Console.ReadLine());
+
+            Console.WriteLine("Phone number:");
+            Contactlist.Add("Phone number", Console.ReadLine());
+
+            Console.WriteLine("Email:");
+            Contactlist.Add("Email", Console.ReadLine());
+        }
+    }
+}
